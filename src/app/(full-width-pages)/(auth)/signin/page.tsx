bc2ad33778b0ea6ce78b30aed8a6c742bc8681b3
maearon <manhng132@gmail.com>
2025-09-21 19:21:19 +0700

    if (user) redirect("/");

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -14,7 +14,7 @@ export default async function SignIn() {
   const session = await getServerSession();
   const user = session?.user;
 
-  if (!user) redirect("/signin");
+  if (user) redirect("/");
 
   return <SignInForm />;
 }
