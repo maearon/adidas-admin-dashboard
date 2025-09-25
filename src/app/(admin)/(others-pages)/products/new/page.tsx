e0dedfb88656720f7e47b3b075cb5cc9df98c2eb
maearon <manhng132@gmail.com>
2025-09-25 17:25:58 +0700

    NewProductPage

--- src/app/(admin)/(others-pages)/products/new/page.tsx
+++ src/app/(admin)/(others-pages)/products/new/page.tsx
@@ -2,9 +2,7 @@ import { Suspense } from "react";
 import { Metadata } from "next";
 import NewProductPageClient from "./NewProductPageClient";
 import { formatSlugTitle } from "@/utils/category-config.auto";
-import Loading from "@/components/loading";
-import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import ComponentCard from "@/components/common/ComponentCard";
+import { Loading } from "@/components/loading";
 
 // ✅ generateMetadata must be async with awaited `params`
 export async function generateMetadata(
@@ -18,17 +16,17 @@ export async function generateMetadata(
 }
 
 // ✅ Main page function must await `params`
-const ProductDetailPage = async () => {
+const NewProductPage = async () => {
   return (
     <div className="min-h-screen bg-background">
       <Suspense fallback={<Loading />}>
-      <PageBreadcrumb pageTitle="New Product" />
-        <ComponentCard title="All Products">
+      {/* <PageBreadcrumb pageTitle="New Product" />
+      <ComponentCard title="All Products"> */}
         <NewProductPageClient />
-        </ComponentCard>
+      {/* </ComponentCard> */}
       </Suspense>
     </div>
   );
 };
 
-export default ProductDetailPage;
+export default NewProductPage;
